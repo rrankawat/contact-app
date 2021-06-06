9312c69b7b302238e4762748ef24cbdf7dfb90c2
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-06-06 22:02:09 +0530

    Redux Init & Store

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Topbar from './components/Topbar';
-
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
-    <Router>
-      <Topbar />
-      <div className="container py-5">
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Topbar />
+        <div className="container py-5">
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
